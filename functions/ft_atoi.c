4596159a0d1e22dfa066287650dbb2728e696dd1
Anselmi Henttonen <ahentton@c1r4p3.hive.fi>
2024-04-18 17:21:25 +0300

    atoi needs work

---
+++ functions/ft_atoi.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahentton <ahentton@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 16:47:55 by ahentton          #+#    #+#             */
+/*   Updated: 2024/04/18 17:21:09 by ahentton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	char	*str1;
+	int		i;
+	int		ret;
+	int		minus;
+
+	str1 = (char *) str;
+	i = 0;
+	ret = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+		i++;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '-')
+			minus = 1;
+		while (str[i] >= '0' && str[i] <= '9')
+		{
+			ret *= 10 + str[i] + '0';
+			i++;
+		}
+	}
+	if (minus == 1)
+		return (ret * (-1));
+	return (ret);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	char	str[] = "2147483648";
+
+	printf("%d\n%d", ft_atoi(str), atoi(str));
+	return (0);
+
+}
