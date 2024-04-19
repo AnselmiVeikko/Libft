b2398bdd2134ebc8bef96c51ae96be5574c6190d
Anselmi Henttonen <ahentton@c1r4p4.hive.fi>
2024-04-19 11:10:53 +0300

    tidied up headerfile

--- libft.h
+++ libft.h
@@ -6,25 +6,25 @@
 /*   By: ahentton <ahentton@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:26:50 by ahentton          #+#    #+#             */
-/*   Updated: 2024/04/19 11:01:30 by ahentton         ###   ########.fr       */
+/*   Updated: 2024/04/19 11:10:34 by ahentton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stdlib.ih>
+# include <stdlib.h>
 
-int	ft_isalpha(int arg);
-int	ft_isdigit(int arg);
-int	ft_isalnum(int arg);
-int	ft_isascii(int arg);
-int	ft_isprint(int arg);
-int	ft_toupper(int c);
-int	ft_tolower(char c);
-int	ft_atoi(const char *str);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_isalpha(int arg);
+int		ft_isdigit(int arg);
+int		ft_isalnum(int arg);
+int		ft_isascii(int arg);
+int		ft_isprint(int arg);
+int		ft_toupper(int c);
+int		ft_tolower(char c);
+int		ft_atoi(const char *str);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
@@ -37,3 +37,4 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
 
+#endif
